963cdcd0603ed35f8cd595a75287011145509013
mlhtnc <mlhtncc@gmail.com>
2018-03-10 21:38:06 +0300

    Some edits and no retweet update.

--- python/streaming.py
+++ python/streaming.py
@@ -9,37 +9,39 @@ import codecs
 import tweepy
 import atexit
 import sys
-filterList=[u'a',u'üzgün',u'sinirli',u'agresif',u'hüzünlü',u'mutsuz',u'hüzün',u'sakin'  ]
-#Variables that contains the user credentials to access Twitter API
 
-#nothing is correct
-access_token = "sdf-dsf"
-access_token_secret = "sdf"
-consumer_key = "sdf"
-consumer_secret = "dsf"
-#nothing is correct
-counter = 0
+access_token = "XXXXXXXXXXXX-XXXXXXXXXXXXX"
+access_token_secret = "XXXXXXXXXXXXXXXXXXXX"
+consumer_key = "XXXXXXXXXXXXXXXXXX"
+consumer_secret = "XXXXXXXXXXXXXXXXXXXXX"
+
 listOfData=[]
 f = open("tweets.json","w")
+
+filterList=[u'a',u'üzgün',u'sinirli',u'agresif',u'hüzünlü',u'mutsuz',u'hüzün',u'sakin'  ]
+
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
-    countData=0
+
     def on_status(self, status):
         try:
             text = status.extended_tweet["full_text"].encode('utf-8')
-            #f.write("Username: " + status.user.screen_name +"\n")
-            data={'Tweet': text }
-            listOfData.append(data)
-            print text
-            #f.flush()
-            if len(listOfData) >= 10:
-                sys.exitfunc()
-                
-
         except AttributeError:
-            text = status.text    
+            text = status.text.encode('utf-8')
+        
+        if(text.startswith('RT')):
+            return
+        
+        data = {'Tweet': text }
+        listOfData.append(data)
+        print text
+
+        if len(listOfData) >= 20:
+            sys.exitfunc()
+
+            
     def on_error(self, status):
-        print(status ,'hatasi')
+        print(status)
 
         
 def saver():
@@ -48,14 +50,15 @@ def saver():
     quit()
 
 if __name__ == '__main__':
-	    
-    #This handles Twitter authetification and the connection to Twitter Streaming API
+    
     atexit.register(saver)
+    
+    #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth,l,tweet_mode = 'extended',truncated= 'false')
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream = Stream(auth,l,tweet_mode = 'extended',truncated = 'false')
+    
     stream.filter(track=filterList,languages = ['tr'])
     
 
